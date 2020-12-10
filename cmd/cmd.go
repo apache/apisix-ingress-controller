0a6f91aa08b3ae8bf610970ca5446fb8ff41f812
tokers <zchao1995@gmail.com>
2020-12-10 17:25:18 +0800

    fix: indent

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,47 +1,47 @@
- // Licensed to the Apache Software Foundation (ASF) under one or more
- // contributor license agreements.  See the NOTICE file distributed with
- // this work for additional information regarding copyright ownership.
- // The ASF licenses this file to You under the Apache License, Version 2.0
- // (the "License"); you may not use this file except in compliance with
- // the License.  You may obtain a copy of the License at
- //
- //     http://www.apache.org/licenses/LICENSE-2.0
- //
- // Unless required by applicable law or agreed to in writing, software
- // distributed under the License is distributed on an "AS IS" BASIS,
- // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- // See the License for the specific language governing permissions and
- // limitations under the License.
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package cmd
 
- import (
-	 "fmt"
+import (
+	"fmt"
 
-	 "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 
-	 "github.com/api7/ingress-controller/cmd/ingress"
-	 "github.com/api7/ingress-controller/pkg/version"
- )
+	"github.com/api7/ingress-controller/cmd/ingress"
+	"github.com/api7/ingress-controller/pkg/version"
+)
 
- func newVersionCommand() *cobra.Command {
-	 return &cobra.Command{
-		 Use:   "version",
-		 Short: "version for apisix-ingress-controller",
-		 Run: func(cmd *cobra.Command, _ []string) {
-			 fmt.Printf("apisix-ingress-controller version %s\n", version.Version())
-		 },
-	 }
- }
+func newVersionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "version for apisix-ingress-controller",
+		Run: func(cmd *cobra.Command, _ []string) {
+			fmt.Printf("apisix-ingress-controller version %s\n", version.Version())
+		},
+	}
+}
 
- // NewAPISIXIngressControllerCommand creates the apisix-ingress-controller command.
- func NewAPISIXIngressControllerCommand() *cobra.Command {
-	 cmd := &cobra.Command{
-		 Use:     "apisix-ingress-controller [command]",
-		 Long:    "Yet another Ingress controller for Kubernetes using Apache APISIX as the high performance reverse proxy. Please note that all flags in this command line is not in use for now, but will be enabled in the near future.",
-		 Version: version.Version(),
-	 }
+// NewAPISIXIngressControllerCommand creates the apisix-ingress-controller command.
+func NewAPISIXIngressControllerCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "apisix-ingress-controller [command]",
+		Long:    "Yet another Ingress controller for Kubernetes using Apache APISIX as the high performance reverse proxy. Please note that all flags in this command line is not in use for now, but will be enabled in the near future.",
+		Version: version.Version(),
+	}
 
-	 cmd.AddCommand(ingress.NewIngressCommand())
-	 cmd.AddCommand(newVersionCommand())
-	 return cmd
- }
+	cmd.AddCommand(ingress.NewIngressCommand())
+	cmd.AddCommand(newVersionCommand())
+	return cmd
+}
