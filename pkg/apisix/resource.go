10dcd4c4f7018341dd14ea3714c0ff4cbc341866
kv <gxthrj@163.com>
2021-01-08 23:30:57 +0800

    fix: apisix can not remove svc sometimes

--- pkg/apisix/resource.go
+++ pkg/apisix/resource.go
@@ -116,6 +116,7 @@ func (i *item) upstream(clusterName string) (*v1.Upstream, error) {
 	}
 
 	var ups upstreamItem
+	log.Info(string(i.Value))
 	if err := json.Unmarshal(i.Value, &ups); err != nil {
 		return nil, err
 	}
