f25d23e298a833c0fa7c5dc76067dcbc45e34518
huahu0213 <huahu0213@gmail.com>
2021-02-04 20:10:59 +0800

    Version information should be shown when the program started #228

--- cmd/ingress/ingress_test.go
+++ cmd/ingress/ingress_test.go
@@ -137,6 +137,9 @@ func TestNewIngressCommandEffectiveLog(t *testing.T) {
 	assert.Contains(t, f.Message, "apisix ingress controller started")
 	assert.Equal(t, f.Level, "info")
 
+	assert.Contains(t, f.Message, "version: ")
+	assert.Equal(t, f.Level, "info")
+
 	f = parseLog(t, buf)
 	assert.Contains(t, f.Message, "use configuration")
 	assert.Equal(t, f.Level, "info")
