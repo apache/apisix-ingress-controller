78cd62ee9c86819524d546f1daff1953ac161006
Alex Zhang <zchao1995@gmail.com>
2021-05-03 10:41:14 +0800

    fix

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -86,10 +86,12 @@ type APISIXConfig struct {
 	// TODO: Obsolete the plain way to specify admin_key, which is insecure.
 	DefaultClusterAdminKey string `json:"default_cluster_admin_key" yaml:"default_cluster_admin_key"`
 	// BaseURL is same to DefaultClusterBaseURL.
-	// Deprecated: use DefaultClusterBaseURL instead.
+	// Deprecated: use DefaultClusterBaseURL instead. BaseURL will be removed
+	// once v1.0.0 is released.
 	BaseURL string `json:"base_url" yaml:"base_url"`
 	// AdminKey is same to DefaultClusterAdminKey.
-	// Deprecated: use DefaultClusterAdminKey instead.
+	// Deprecated: use DefaultClusterAdminKey instead. AdminKey will be removed
+	//	// once v1.0.0 is released.
 	AdminKey string `json:"admin_key" yaml:"admin_key"`
 }
 
