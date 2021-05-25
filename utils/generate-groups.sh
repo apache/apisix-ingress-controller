812c4c579dd7575e7fa1e9b9916fa363f491e691
Ling Samuel <lingsamuelgrace@gmail.com>
2021-05-25 10:26:49 +0800

    chore: add license ignore for generate-groups
    Signed-off-by: Ling Samuel <lingsamuelgrace@gmail.com>

--- utils/generate-groups.sh
+++ utils/generate-groups.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+
+# Copyright 2017 The Kubernetes Authors.
 #
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -14,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 # This script comes from https://github.com/kubernetes/code-generator/blob/master/generate-groups.sh
 
 set -o errexit
