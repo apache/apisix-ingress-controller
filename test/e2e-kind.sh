8d43386e7d23bb4482dde846c2ed9dce77b3c7b4
Fang <37891485+fhuzero@users.noreply.github.com>
2021-06-22 14:41:40 +0800

    chore: renaming var & removing redundant cmd
    * renaming several shell variables to make their name more distinct and clear
    * removing command which replaces apisix.base_url since the default value has been changed in config file
    * removing wait for APISIX deployment command since redundancy

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -4,23 +4,21 @@
 
 set -euo pipefail
 
-APISIX_GATEWAY_NAMESPACE=ingress-apisix
+APISIX_NAMESPACE=ingress-apisix
 
 export KO_DOCKER_REPO=kind.local
 export KIND_CLUSTER_NAME="ingress-apisix-integration"
 $(dirname $0)/upload-test-images.sh
 
 echo ">> Setup test resources"
-# TODO
+# TODO: check whether .yaml files under test/config need to be revised
 ko apply -f test/config
 
 #ip=$(kubectl get nodes -lkubernetes.io/hostname!=kind-control-plane -ojsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' | head -n1)
-export NODE_IP=$(kubectl get nodes --namespace ingress-apisix -o jsonpath="{.items[0].status.addresses[0].address}")
-export NODE_PORT=$(kubectl get --namespace ingress-apisix -o jsonpath="{.spec.ports[0].nodePort}" services apisix-admin)
+export NODE_IP=$(kubectl get nodes --namespace ${APISIX_NAMESPACE} -o jsonpath="{.items[0].status.addresses[0].address}")
 echo
-export APISIX_IP=${NODE_IP}:${NODE_PORT}
 export "GATEWAY_OVERRIDE=apisix"
-export "GATEWAY_NAMESPACE_OVERRIDE=${APISIX_GATEWAY_NAMESPACE}"
+export "GATEWAY_NAMESPACE_OVERRIDE=${APISIX_NAMESPACE}"
 
 echo ">> Running conformance tests"
 # timeout is 1m to failfast since the test always timed out
--- utils/setup.sh
+++ utils/setup.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 KNATIVE_NAMESPACE=knative-serving
-APISIX_GATEWAY_NAMESPACE=ingress-apisix
+APISIX_NAMESPACE=ingress-apisix
 
 export KIND_CLUSTER_NAME="ingress-apisix-integration"
 kind delete cluster
@@ -17,34 +17,31 @@ kubectl patch configmap/config-network -n ${KNATIVE_NAMESPACE} --type merge -p '
 
 export KO_DOCKER_REPO=kind.local
 
-# TODO
-kubectl create namespace ${APISIX_GATEWAY_NAMESPACE}
+kubectl create namespace ${APISIX_NAMESPACE}
 echo "Deploying APISIX"
 # Should already be installed by Helm, check kubectl to change service type to NodePort
 helm install apisix apisix/apisix \
   --set admin.allow.ipList="{0.0.0.0/0}" \
-  --namespace ${APISIX_GATEWAY_NAMESPACE}
-kubectl patch svc apisix-admin --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' -n=${APISIX_GATEWAY_NAMESPACE}
-echo "Wait for APISIX deployment to be up (timeout=300s)"
-kubectl -n ${APISIX_GATEWAY_NAMESPACE} wait --timeout=300s --for=condition=Available deployments --all
+  --namespace ${APISIX_NAMESPACE}
+#kubectl patch svc apisix-admin --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' -n=${APISIX_NAMESPACE}
 #ip=$(kubectl get nodes -lkubernetes.io/hostname!=kind-control-plane -ojsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' | head -n1)
 #port=$(kubectl -n ingress-apisix get svc apisix -ojsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 export NODE_IP=$(kubectl get nodes --namespace ingress-apisix -o jsonpath="{.items[0].status.addresses[0].address}")
-export NODE_PORT=$(kubectl get --namespace ingress-apisix -o jsonpath="{.spec.ports[0].nodePort}" services apisix-admin)
+export ADMIN_PORT=$(kubectl get --namespace ingress-apisix -o jsonpath="{.spec.ports[0].nodePort}" services apisix-admin)
+export GATEWAY_PORT=$(kubectl get --namespace ingress-apisix -o jsonpath="{.spec.ports[0].nodePort}" services apisix-gateway)
 echo
-export APISIX_IP=${NODE_IP}:${NODE_PORT}
-echo "You can connect to APISIX Gateway at ${APISIX_IP}"
-echo "Example usage: 'curl \"http://${APISIX_IP}/apisix/admin/services/\" -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1'"
+export APISIX_ADMIN_AUTHORITY=${NODE_IP}:${ADMIN_PORT}
+export APISIX_GATEWAY_AUTHORITY=${NODE_IP}:${GATEWAY_PORT}
+echo "You can connect to APISIX Admin at ${APISIX_ADMIN_AUTHORITY}"
+echo "Example usage: 'curl \"http://${APISIX_ADMIN_AUTHORITY}/apisix/admin/services/\" -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1'"
 
 echo "Deploying APISIX ingress controller"
 # https://github.com/google/ko#does-ko-work-with-kustomize
 # https://github.com/apache/apisix-ingress-controller/blob/master/install.md#kustomize-support
-
 kubectl kustomize samples/deploy |
   sed 's/LoadBalancer/NodePort/g' |
-  sed 's@http://127.0.0.1:9080/apisix/admin@http://${APISIX_IP}/apisix/admin@g' |
   ko resolve -f - |
   kubectl apply -f -
 
 echo "Wait for APISIX ingress controller deployment to be up (timeout=300s)"
-kubectl -n ${APISIX_GATEWAY_NAMESPACE} wait --timeout=300s --for=condition=Available deployments --all
+kubectl -n ${APISIX_NAMESPACE} wait --timeout=300s --for=condition=Available deployments --all
