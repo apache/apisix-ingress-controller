8f9404cb1875c6f0e2b2ea863e82fb18d0f794b0
qianyong <qianyong@api7.ai>
2021-10-15 11:35:12 +0800

    feat: support environment variable in config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -15,10 +15,14 @@
 package config
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -137,10 +141,27 @@ func NewConfigFromFile(filename string) (*Config, error) {
 		return nil, err
 	}
 
+	envVarMap := map[string]string{}
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		envVarMap[pair[0]] = pair[1]
+	}
+
+	tpl := template.New("text").Option("missingkey=error")
+	tpl, err = tpl.Parse(string(data))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing configuration template %v", err)
+	}
+	buf := bytes.NewBufferString("")
+	err = tpl.Execute(buf, envVarMap)
+	if err != nil {
+		return nil, fmt.Errorf("error execute configuration template %v", err)
+	}
+
 	if strings.HasSuffix(filename, ".yaml") || strings.HasSuffix(filename, ".yml") {
-		err = yaml.Unmarshal(data, cfg)
+		err = yaml.Unmarshal(buf.Bytes(), cfg)
 	} else {
-		err = json.Unmarshal(data, cfg)
+		err = json.Unmarshal(buf.Bytes(), cfg)
 	}
 
 	if err != nil {
