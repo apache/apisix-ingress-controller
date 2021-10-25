a02c42e1376c8980c48f0942be9d74a172119842
kv <gxthrj@163.com>
2021-10-25 21:47:33 +0800

    use ginkgo.Describe

--- test/e2e/ingress/status.go
+++ test/e2e/ingress/status.go
@@ -25,7 +25,7 @@ import (
 	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
 )
 
-var _ = ginkgo.FDescribe("Status subresource Testing", func() {
+var _ = ginkgo.Describe("Status subresource Testing", func() {
 	opts := &scaffold.Options{
 		Name:                  "default",
 		Kubeconfig:            scaffold.GetKubeconfig(),
