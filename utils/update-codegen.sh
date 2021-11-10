28ef132d30ba7a43461bc6b9af171c099b849358
kv <gxthrj@163.com>
2021-11-10 20:17:15 +0800

    add v2beta3

--- utils/update-codegen.sh
+++ utils/update-codegen.sh
@@ -31,7 +31,7 @@ rm -rf "$GENERATED_ROOT"
 
 bash "${SCRIPT_ROOT}"/generate-groups.sh "deepcopy,client,informer,lister" \
   ${PKG_NAME}/pkg/kube/apisix/client ${PKG_NAME}/pkg/kube/apisix/apis \
-  config:v1,v2alpha1,v2beta1,v2beta2 ${PKG_NAME} \
+  config:v2beta1,v2beta2,v2beta3 ${PKG_NAME} \
   --output-base "$GENERATED_ROOT" \
   --go-header-file "${SCRIPT_ROOT}"/boilerplate.go.txt \
   "$@"
