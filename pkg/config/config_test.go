a46283a813b46c0a453a5e3505323e7240a985d1
Jintao Zhang <zhangjintao9020@gmail.com>
2021-12-02 09:34:20 +0800

    fix: config unit test
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -109,13 +109,15 @@ apisix:
 
 func TestConfigWithEnvVar(t *testing.T) {
 	cfg := &Config{
-		LogLevel:        "warn",
-		LogOutput:       "stdout",
-		HTTPListen:      ":9090",
-		HTTPSListen:     ":9443",
-		CertFilePath:    "/etc/webhook/certs/cert.pem",
-		KeyFilePath:     "/etc/webhook/certs/key.pem",
-		EnableProfiling: true,
+		LogLevel:              "warn",
+		LogOutput:             "stdout",
+		HTTPListen:            ":9090",
+		HTTPSListen:           ":9443",
+		IngressPublishService: "",
+		IngressStatusAddress:  []string{},
+		CertFilePath:          "/etc/webhook/certs/cert.pem",
+		KeyFilePath:           "/etc/webhook/certs/key.pem",
+		EnableProfiling:       true,
 		Kubernetes: KubernetesConfig{
 			ResyncInterval:     types.TimeDuration{Duration: time.Hour},
 			Kubeconfig:         "",
@@ -147,6 +149,8 @@ func TestConfigWithEnvVar(t *testing.T) {
     "log_output": "stdout",
     "http_listen": ":9090",
     "https_listen": ":9443",
+	"ingress_publish_service": "",
+	"ingress_status_address": [],
     "enable_profiling": true,
     "kubernetes": {
         "kubeconfig": "{{.KUBECONFIG}}",
@@ -180,6 +184,8 @@ log_level: warn
 log_output: stdout
 http_listen: :9090
 https_listen: :9443
+ingress_publish_service: ""
+ingress_status_address: []
 enable_profiling: true
 kubernetes:
   resync_interval: 1h0m0s
