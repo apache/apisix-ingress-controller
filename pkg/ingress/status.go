70de5b55d3ef0f80ab356e08056289bfdbd7d316
qianyong <qianyong@api7.ai>
2022-01-03 21:56:49 +0800

    fix: objects get from lister must be treated as read-only

--- pkg/ingress/status.go
+++ pkg/ingress/status.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/cache"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 
@@ -69,6 +70,10 @@ func (c *Controller) recordStatus(at interface{}, reason string, err error, stat
 	}
 	client := c.kubeClient.APISIXClient
 	kubeClient := c.kubeClient.Client
+
+	if kubeObj, ok := at.(runtime.Object); ok {
+		at = kubeObj.DeepCopyObject()
+	}
 
 	switch v := at.(type) {
 	case *configv2beta3.ApisixTls:
