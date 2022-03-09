4025151e44b9f64aa0881248faa3088487b53ec6
JasonZhu <491537461q@gmail.com>
2022-03-09 11:06:40 +0800

    feat: format gin logger (#904)

---
+++ pkg/log/gin_logger.go
@@ -0,0 +1,86 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package log
+
+import (
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GinLogger receive gin The default log of the framework
+func GinLogger(logger *Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+		cost := time.Since(start)
+		if c.Writer.Status() >= 400 && c.Writer.Status() <= 599 {
+			logger.Errorf("path: %s, status: %d, method: %s, query: %s, ip: %s, user-agent: %s, errors: %s, cost: %s",
+				path, c.Writer.Status(), c.Request.Method, query, c.ClientIP(), c.Request.UserAgent(), c.Errors.ByType(gin.ErrorTypePrivate).String(), cost)
+		} else {
+			logger.Infof("path: %s, status: %d, method: %s, query: %s, ip: %s, user-agent: %s, errors: %s, cost: %s",
+				path, c.Writer.Status(), c.Request.Method, query, c.ClientIP(), c.Request.UserAgent(), c.Errors.ByType(gin.ErrorTypePrivate).String(), cost)
+		}
+	}
+}
+
+// GinRecovery recover Drop the project that may appear panic
+func GinRecovery(logger *Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") ||
+							strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					logger.Errorf("path: %s, error: %s, request: %s",
+						c.Request.URL.Path, err, string(httpRequest))
+					// If the connection is dead, we can't write a status to it.
+					c.Error(err.(error)) // nolint: errcheck
+					c.Abort()
+
+					return
+				}
+				if stack {
+					logger.Errorf("error: [Recovery from panic] %s, request: %s, stack: %s",
+						err, string(httpRequest), string(debug.Stack()),
+					)
+				} else {
+					logger.Errorf("error: [Recovery from panic] %s, request: %s",
+						err, string(httpRequest))
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
+	}
+}
---
+++ pkg/log/gin_logger_test.go
@@ -0,0 +1,124 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package log
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+const TestMode = "test"
+
+func init() {
+	gin.SetMode(TestMode)
+}
+
+type header struct {
+	Key   string
+	Value string
+}
+
+func performRequest(r http.Handler, method, path string, headers ...header) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(method, path, nil)
+	for _, h := range headers {
+		req.Header.Add(h.Key, h.Value)
+	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestGinLogger(t *testing.T) {
+	t.Run("test log with gin logger", func(t *testing.T) {
+		fws := &fakeWriteSyncer{}
+		logger, err := NewLogger(WithLogLevel("debug"), WithWriteSyncer(fws))
+		assert.Nil(t, err, "failed to new logger: ", err)
+		defer logger.Close()
+
+		router := gin.New()
+		router.Use(GinLogger(logger))
+		router.GET("/healthz", func(c *gin.Context) {})
+		performRequest(router, "GET", "/healthz")
+		res := string(fws.bytes())
+		assert.Contains(t, res, "200")
+		assert.Contains(t, res, "GET")
+		assert.Contains(t, res, "/healthz")
+	})
+	t.Run("test log with gin logger 4xx and 5xx", func(t *testing.T) {
+		fws := &fakeWriteSyncer{}
+		logger, err := NewLogger(WithLogLevel("debug"), WithWriteSyncer(fws))
+		assert.Nil(t, err, "failed to new logger: ", err)
+		defer logger.Close()
+
+		router := gin.New()
+		router.Use(GinLogger(logger))
+		router.GET("/healthz", func(c *gin.Context) { c.JSON(500, nil) })
+		performRequest(router, "GET", "/healthz")
+		res := string(fws.bytes())
+		assert.Contains(t, res, "500")
+		assert.Contains(t, res, "GET")
+		assert.Contains(t, res, "/healthz")
+		assert.Contains(t, res, "error")
+		router.GET("/healthz-check", func(c *gin.Context) { c.JSON(400, nil) })
+		performRequest(router, "GET", "/healthz-check")
+		res = string(fws.bytes())
+		assert.Contains(t, res, "400")
+		assert.Contains(t, res, "GET")
+		assert.Contains(t, res, "/healthz-check")
+		assert.Contains(t, res, "error")
+	})
+}
+
+func TestGinRecovery(t *testing.T) {
+	t.Run("test log with gin recovery with stack", func(t *testing.T) {
+		fws := &fakeWriteSyncer{}
+		logger, err := NewLogger(WithLogLevel("debug"), WithWriteSyncer(fws))
+		assert.Nil(t, err, "failed to new logger: ", err)
+		defer logger.Close()
+
+		router := gin.New()
+		router.Use(GinRecovery(logger, true))
+		router.GET("/healthz", func(c *gin.Context) { panic("test log with gin recovery") })
+		performRequest(router, "GET", "/healthz")
+		res := string(fws.bytes())
+		fmt.Println(res)
+		assert.Contains(t, res, "caller")
+		assert.Contains(t, res, "GET")
+		assert.Contains(t, res, "/healthz")
+		assert.Contains(t, res, "stack")
+	})
+	t.Run("test log with gin recovery", func(t *testing.T) {
+		fws := &fakeWriteSyncer{}
+		logger, err := NewLogger(WithLogLevel("debug"), WithWriteSyncer(fws))
+		assert.Nil(t, err, "failed to new logger: ", err)
+		defer logger.Close()
+
+		router := gin.New()
+		router.Use(GinRecovery(logger, false))
+		router.GET("/healthz", func(c *gin.Context) { panic("test log with gin recovery") })
+		performRequest(router, "GET", "/healthz")
+		res := string(fws.bytes())
+		fmt.Println(res)
+		assert.Contains(t, res, "caller")
+		assert.Contains(t, res, "GET")
+		assert.Contains(t, res, "/healthz")
+		assert.NotContains(t, res, "stack")
+	})
+}
