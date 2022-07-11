a73b52d3946dcc08473888445a0c66152e659120
Xin Rong <1324266492@qq.com>
2022-07-11 13:19:50 +0800

    feat:  support endpointslice, and improve test/e2e/endpoints.go tests. (#1140)

--- test/e2e/suite-endpoints/endpoints.go
+++ test/e2e/suite-endpoints/endpoints.go
@@ -17,7 +17,6 @@ package endpoints
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	ginkgo "github.com/onsi/ginkgo/v2"
 	"github.com/stretchr/testify/assert"
@@ -28,10 +27,8 @@ import (
 var _ = ginkgo.Describe("suite-endpoints: endpoints", func() {
 	suites := func(s *scaffold.Scaffold) {
 		ginkgo.It("ignore applied only if there is an ApisixRoute referenced", func() {
-			time.Sleep(5 * time.Second)
-			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumApisixUpstreamsCreated(0), "checking number of upstreams")
 			backendSvc, backendSvcPort := s.DefaultHTTPBackend()
-			ups := fmt.Sprintf(`
+			ar := fmt.Sprintf(`
 apiVersion: apisix.apache.org/v2beta3
 kind: ApisixRoute
 metadata:
@@ -48,12 +45,14 @@ spec:
     - serviceName: %s
       servicePort: %d
 `, backendSvc, backendSvcPort[0])
-			assert.Nil(ginkgo.GinkgoT(), s.CreateResourceFromString(ups))
+			assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(ar))
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumApisixRoutesCreated(1), "checking number of routes")
 			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumApisixUpstreamsCreated(1), "checking number of upstreams")
+
+			s.NewAPISIXClient().GET("/ip").WithHeader("Host", "httpbin.org").Expect().Status(http.StatusOK)
 		})
 
 		ginkgo.It("upstream nodes should be reset to empty when Service/Endpoints was deleted", func() {
-			ginkgo.Skip("now we don't handle endpoints delete event")
 			backendSvc, backendSvcPort := s.DefaultHTTPBackend()
 			apisixRoute := fmt.Sprintf(`
 apiVersion: apisix.apache.org/v2beta3
@@ -66,18 +65,48 @@ spec:
     match:
       hosts:
       - httpbin.com
-      paths: /ip
+      paths:
+      - /ip
     backends:
     - serviceName: %s
       servicePort: %d
 `, backendSvc, backendSvcPort[0])
-			assert.Nil(ginkgo.GinkgoT(), s.CreateResourceFromString(apisixRoute))
+			assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apisixRoute))
 			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumApisixUpstreamsCreated(1), "checking number of upstreams")
 			s.NewAPISIXClient().GET("/ip").WithHeader("Host", "httpbin.com").Expect().Status(http.StatusOK)
 
 			// Now delete the backend httpbin service resource.
 			assert.Nil(ginkgo.GinkgoT(), s.DeleteHTTPBINService())
-			time.Sleep(3 * time.Second)
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumListUpstreamNodesNth(1, 0))
+			s.NewAPISIXClient().GET("/ip").WithHeader("Host", "httpbin.com").Expect().Status(http.StatusServiceUnavailable)
+		})
+
+		ginkgo.It("when endpoint is 0, upstream nodes is also 0", func() {
+			backendSvc, backendSvcPort := s.DefaultHTTPBackend()
+			apisixRoute := fmt.Sprintf(`
+apiVersion: apisix.apache.org/v2beta3
+kind: ApisixRoute
+metadata:
+ name: httpbin-route
+spec:
+  http:
+  - name: rule1
+    match:
+      hosts:
+      - httpbin.com
+      paths: 
+      - /ip
+    backends:
+    - serviceName: %s
+      servicePort: %d
+`, backendSvc, backendSvcPort[0])
+			assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apisixRoute))
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumApisixRoutesCreated(1), "checking number of upstreams")
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumListUpstreamNodesNth(1, 1))
+
+			// scale HTTPBIN, so the endpoints controller has the opportunity to update upstream.
+			assert.Nil(ginkgo.GinkgoT(), s.ScaleHTTPBIN(0))
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumListUpstreamNodesNth(1, 0))
 			s.NewAPISIXClient().GET("/ip").WithHeader("Host", "httpbin.com").Expect().Status(http.StatusServiceUnavailable)
 		})
 	}
@@ -110,25 +139,21 @@ spec:
     - serviceName: %s
       servicePort: %d
 `, backendSvc, backendSvcPort[0])
-			assert.Nil(ginkgo.GinkgoT(), s.CreateResourceFromString(apisixRoute))
-			time.Sleep(12 * time.Second)
-			ups, err := s.ListApisixUpstreams()
-			assert.Nil(ginkgo.GinkgoT(), err, "listing APISIX upstreams")
-			assert.Len(ginkgo.GinkgoT(), ups, 1)
-			assert.Len(ginkgo.GinkgoT(), ups[0].Nodes, 1)
+			assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apisixRoute))
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumListUpstreamNodesNth(1, 1))
 
 			// port in nodes is still the targetPort, not the service port
+			ups, err := s.ListApisixUpstreams()
+			assert.Nil(ginkgo.GinkgoT(), err, "listing APISIX upstreams")
 			assert.Equal(ginkgo.GinkgoT(), ups[0].Nodes[0].Port, 80)
 
 			// scale HTTPBIN, so the endpoints controller has the opportunity to update upstream.
 			assert.Nil(ginkgo.GinkgoT(), s.ScaleHTTPBIN(3))
-			time.Sleep(30 * time.Second)
-			ups, err = s.ListApisixUpstreams()
-			assert.Nil(ginkgo.GinkgoT(), err, "listing APISIX upstreams")
-			assert.Len(ginkgo.GinkgoT(), ups, 1)
-			assert.Len(ginkgo.GinkgoT(), ups[0].Nodes, 3)
+			assert.Nil(ginkgo.GinkgoT(), s.EnsureNumListUpstreamNodesNth(1, 3))
 
 			// port in nodes is still the targetPort, not the service port
+			ups, err = s.ListApisixUpstreams()
+			assert.Nil(ginkgo.GinkgoT(), err, "listing APISIX upstreams")
 			assert.Equal(ginkgo.GinkgoT(), ups[0].Nodes[0].Port, 80)
 			assert.Equal(ginkgo.GinkgoT(), ups[0].Nodes[1].Port, 80)
 			assert.Equal(ginkgo.GinkgoT(), ups[0].Nodes[2].Port, 80)
