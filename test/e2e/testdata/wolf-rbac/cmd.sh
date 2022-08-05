8f5a0be61be7634825ce5f5b924d4d529e570516
zaunist <y.bz@foxmail.com>
2022-08-05 14:51:03 +0800

    feat: check docker compose command

--- test/e2e/testdata/wolf-rbac/cmd.sh
+++ test/e2e/testdata/wolf-rbac/cmd.sh
@@ -20,25 +20,26 @@
 cd test/e2e/testdata/wolf-rbac/
 
 OPTION=$1
+COMPOSE="docker-compose"
 
 if  ! command -v "docker-compose" > /dev/null; then
-	echo "docker-compose not found, please install docker-compose"
-    exit 0
+	echo "docker-compose not found, try docker compose"
+    COMPOSE="docker compose"
 fi
 
 if  [ $OPTION = "ip" ]; then
     echo -n `docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' wolf-server`
 elif [ $OPTION = "start" ]; then
-    docker-compose -f 'docker-compose.yaml'  -p 'wolf-rbac' down
+    eval "$COMPOSE -f 'docker-compose.yaml'  -p 'wolf-rbac' down"
     rm -rf db-psql.sql
 
     wget https://raw.githubusercontent.com/iGeeky/wolf/f6ddeb75a37bff90406f0f0a2b7ae5d16f6f3bd4/server/script/db-psql.sql
 
     # start database
-    docker-compose up -d database
+    eval "$COMPOSE up -d database"
 
     # start wolf-server
-    docker-compose up -d server restful-demo agent-or agent-demo
+    eval "$COMPOSE up -d server restful-demo agent-or agent-demo"
 
     sleep 6
 
@@ -73,7 +74,7 @@ elif [ $OPTION = "start" ]; then
         "appIDs": ["test-app"]
     }'
 elif [ $OPTION = "stop" ]; then
-    docker-compose -f 'docker-compose.yaml'  -p 'wolf-rbac' down
+    eval "$COMPOSE -f 'docker-compose.yaml'  -p 'wolf-rbac' down"
     rm -rf db-psql.sql
 else
     echo "argument is one of [ip, start, stop]"
