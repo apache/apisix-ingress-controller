78afcc166f2f573dc44f6bd4fec7124c16113fa2
mango <xu.weiKyrie@foxmail.com>
2022-09-23 12:57:22 +0800

    tranfer `host` in apisixroute to `sni`
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

--- pkg/providers/apisix/translation/apisix_route.go
+++ pkg/providers/apisix/translation/apisix_route.go
@@ -836,6 +836,7 @@ func (t *translator) translateStreamRouteV2(ctx *translation.TranslateContext, a
 		name := apisixv1.ComposeStreamRouteName(ar.Namespace, ar.Name, part.Name)
 		sr.ID = id.GenID(name)
 		sr.ServerPort = part.Match.IngressPort
+		sr.SNI = part.Match.Host
 		ups, err := t.translateService(ar.Namespace, backend.ServiceName, backend.Subset, backend.ResolveGranularity, svcClusterIP, svcPort)
 		if err != nil {
 			return err
@@ -901,6 +902,7 @@ func (t *translator) translateStreamRouteNotStrictlyV2(ctx *translation.Translat
 		name := apisixv1.ComposeStreamRouteName(ar.Namespace, ar.Name, part.Name)
 		sr.ID = id.GenID(name)
 		sr.ServerPort = part.Match.IngressPort
+		sr.SNI = part.Match.Host
 		ups, err := t.translateUpstreamNotStrictly(ar.Namespace, backend.ServiceName, backend.Subset, backend.ServicePort.IntVal, backend.ResolveGranularity)
 		if err != nil {
 			return err
