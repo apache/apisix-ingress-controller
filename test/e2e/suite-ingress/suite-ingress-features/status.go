f80fa20463a0454201ea2ee760187d10ec6e0052
rongxin <alinsran@apache.org>
2023-02-02 10:21:34 +0800

    update e2e test

--- test/e2e/suite-ingress/suite-ingress-features/status.go
+++ test/e2e/suite-ingress/suite-ingress-features/status.go
@@ -91,19 +91,14 @@ spec:
       servicePort: %d
 `, backendSvc, backendSvcPort[0])
 		assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apisixRoute))
-
-		err := s.EnsureNumApisixRoutesCreated(1)
-		assert.Nil(ginkgo.GinkgoT(), err, "Checking number of routes")
-		err = s.EnsureNumApisixUpstreamsCreated(1)
-		assert.Nil(ginkgo.GinkgoT(), err, "Checking number of upstreams")
-		time.Sleep(4 * time.Second)
+		time.Sleep(6 * time.Second)
 		// status should be recorded as successful
 		output, err := s.GetOutputFromString("ar", "httpbin-route", "-o", "yaml")
 		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixRoute resource")
-		hasType := strings.Contains(output, "type: ResourcesAvailable")
-		assert.True(ginkgo.GinkgoT(), hasType, "Status is recorded")
-		hasMsg := strings.Contains(output, "message: Sync Successfully")
-		assert.True(ginkgo.GinkgoT(), hasMsg, "Status is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourcesSynced", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "True"`, "status.conditions.status  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "message: Sync Successfully", "status.conditions.message  is recorded")
 
 		apisixRoute = fmt.Sprintf(`
 apiVersion: apisix.apache.org/v2
@@ -126,20 +121,18 @@ spec:
       servicePort: %d
 `, backendSvc, backendSvcPort[0])
 		assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apisixRoute))
-
 		time.Sleep(6 * time.Second)
 		// status should be recorded as successful
 		output, err = s.GetOutputFromString("ar", "httpbin-route", "-o", "yaml")
 		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixRoute resource")
-		hasType = strings.Contains(output, "type: ResourceSyncAborted")
-		assert.True(ginkgo.GinkgoT(), hasType, "Status is recorded")
-		hasMsg = strings.Contains(output, "synced failed, with error")
-		assert.True(ginkgo.GinkgoT(), hasMsg, "Status is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourceSyncAborted", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "False"`, "status.conditions.status  is recorded")
+
 	})
 
 	ginkgo.It("check ApisixPluginConfig status is recorded", func() {
-		backendSvc, backendSvcPort := s.DefaultHTTPBackend()
-		apc := fmt.Sprint(`
+		apc := `
 apiVersion: apisix.apache.org/v2
 kind: ApisixPluginConfig
 metadata:
@@ -150,19 +143,18 @@ spec:
    enable: true
    config:
     body: "my custom body"
-`, backendSvc, backendSvcPort[0])
+`
 		assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apc))
-
 		time.Sleep(6 * time.Second)
 		// status should be recorded as successfulen
-		output, err := s.GetOutputFromString("au", "test-apc", "-o", "yaml")
+		output, err := s.GetOutputFromString("apc", "test-apc", "-o", "yaml")
 		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixPluginConfig resource")
-		hasType := strings.Contains(output, "type: ResourcesAvailable")
-		assert.True(ginkgo.GinkgoT(), hasType, "Status is recorded")
-		hasMsg := strings.Contains(output, "message: Sync Successfully")
-		assert.True(ginkgo.GinkgoT(), hasMsg, "Status is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourcesSynced", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "True"`, "status.conditions.status  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "message: Sync Successfully", "status.conditions.message  is recorded")
 
-		apc = fmt.Sprint(`
+		apc = `
 apiVersion: apisix.apache.org/v2
 kind: ApisixPluginConfig
 metadata:
@@ -175,19 +167,98 @@ spec:
     body: "my custom body"
  - name: non-existent
    enable: true
-`, backendSvc, backendSvcPort[0])
+`
 
 		assert.Nil(ginkgo.GinkgoT(), s.CreateVersionedApisixResource(apc))
+		time.Sleep(6 * time.Second)
+		// status should be recorded as failed
+		output, err = s.GetOutputFromString("apc", "test-apc", "-o", "yaml")
+		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixPluginConfig resource")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourceSyncAborted", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "False"`, "status.conditions.status  is recorded")
+	})
 
+	ginkgo.It("check ApisixTls status is recorded", func() {
+		secretName := "test-apisix-tls"
+		cert := `-----BEGIN CERTIFICATE-----
+MIIDSjCCAjICCQC/34ZwGz7ZXjANBgkqhkiG9w0BAQsFADBmMQswCQYDVQQGEwJD
+TjEQMA4GA1UECAwHSmlhbmdzdTEPMA0GA1UEBwwGU3V6aG91MQ8wDQYDVQQKDAZ6
+aGlsaXUxEDAOBgNVBAsMB3NlY3Rpb24xETAPBgNVBAMMCHRlc3QuY29tMCAXDTIx
+MDIwMzE0MjkwOVoYDzIwNTEwMTI3MTQyOTA5WjBmMQswCQYDVQQGEwJDTjEQMA4G
+A1UECAwHSmlhbmdzdTEPMA0GA1UEBwwGU3V6aG91MQ8wDQYDVQQKDAZ6aGlsaXUx
+EDAOBgNVBAsMB3NlY3Rpb24xETAPBgNVBAMMCHRlc3QuY29tMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3DEQ5K9PVYicINTHt3arqrsrftrhotyBuGqM
+xxqGMVO/E2SAa/81fC1UCcjYV4Wila0kl8i5fa8HjtVm5UWlrqxeFLOS3E0Wv2QY
+w46BGZJY4InE9zKwYyC2DkBxE6p14JRjmtW/MQPNaOFjJ4bmCuRHsEzmQIGRM0b7
+oKHjfFwv6l7BahgGf9ShHOMdHSkgWj6+2RU3282lrO9bY1JBTKu2Znv9M79nu1Px
+Tn1wCfcuCwA7WQT/QSrE2R43I2vmbIbuSmeg9ivjMazRYQQ+qxQn/6zhiHvP3QZG
+dKmp8imdYi+r84PKOLDEe/yxlgIdr2Au5WCPWwyYMYPWHzeD1wIDAQABMA0GCSqG
+SIb3DQEBCwUAA4IBAQBYzNe83mPVuz96TZ3fmxtOIuz9b6q5JWiJiOzjAD9902Se
+TNYzMM6T/5e0dBpj8Z2qQlhkfNxJJgTwGEE8SdrZIr8DhswR9a0bXDCZjLatCdeU
+iYpt+TDAuySnLhAcd3GfE5ml6am2dOsOKpxHU/8clUSaz+21fckRopWo+xL6rSVC
+4vvKqiU+LWLTZPQNoOqowl7bxoQO2jMWfN/5zvQOFxAbEufIPa9ti3qonDCXbkYn
+PpET/mPDrcb4bGsZkW/cu0LrPSUVp12br5TAYaXqYS0Ex+jAVTXML9SeEQuvU3dH
+5Uw2wVHxQXHglsdCYUXXFd3HZffb4rSQH+Mk0CBI
+-----END CERTIFICATE-----`
+		key := `-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA3DEQ5K9PVYicINTHt3arqrsrftrhotyBuGqMxxqGMVO/E2SA
+a/81fC1UCcjYV4Wila0kl8i5fa8HjtVm5UWlrqxeFLOS3E0Wv2QYw46BGZJY4InE
+9zKwYyC2DkBxE6p14JRjmtW/MQPNaOFjJ4bmCuRHsEzmQIGRM0b7oKHjfFwv6l7B
+ahgGf9ShHOMdHSkgWj6+2RU3282lrO9bY1JBTKu2Znv9M79nu1PxTn1wCfcuCwA7
+WQT/QSrE2R43I2vmbIbuSmeg9ivjMazRYQQ+qxQn/6zhiHvP3QZGdKmp8imdYi+r
+84PKOLDEe/yxlgIdr2Au5WCPWwyYMYPWHzeD1wIDAQABAoIBAQDGmSKpgE1H0k0v
+d3siyFART3vtkLHOWKBPmxqaQhwixWwjq5QA1FCDTcbshFBMsGVyJpZIqGxVJdbl
+RyjlRaooH6NDfKvLM2R+/2Mujot2av7qlpgmdXuODOTnecwDds2W33/vGTa2mL1e
+CVuLPSqjTD40j0dlivdRjoZJ3Xn2oOrpZ812XU8KeZAjuSEAwcyl2nSbyLGDchBB
+kfYZold3FaaLAf2LoVJ2fs+FwEPzDKoNYEvij9OyC0kwI94T5jQ+Z6XGtHXhb2Hy
+Ek3EZeIhV3YcDIid5AjSvcrNtDI24hwszSmhYVc53EKYkpXHf581a3U/SEEhXDlw
+Y0x6j9QRAoGBAPEP0LDgP7DGXxno4h+nf0AMru0pxlrNVQhLcNQB+dYI0pFTwsg+
+AKenoaaE/EGR1KfiY0uf3rVWNrA5kyX1/i18iJx9LSf9NvNgMo84JVvXINgyE6sd
+hvdqxFlV5FBnh8b7ldvYQy3YI0EQNx+/rmeUYPjInbkdiksAtAey4ADNAoGBAOnW
+K0FoX1ljq3rc9uVasiRi6Ix50NHdZ17RcEpMgwWPenbP1aiWkvA8yFhU708lBaZC
+WIUZ6XbfiG0Y9cMtxhtikoouDs5Ifia8juZ2bhkmSGP2FvZCBJJ/sHhnhpzSZNhW
+SyLBUjnynoXwHoQvkoGnVTHAk1VsY7jLNJdr2MczAoGAMYvMmu4caRr8pPimsVbd
+4q44reouKK+XUJMg55JYZVN+4/vRRxLnU44yvWUL6/YrPS5ctkhvn9nOd739rom2
+6mZ0NaXMyDFVQAR/n8wscYnv6D+ypzL0cJnzLWFoAdalo5JGJN94P03zQQYyLkZZ
+dFSc8cVaFZgqumu0lPiA7ekCgYEAiMeVL8Jcm84YXVrpNMmzkGMmwhzzT/8hWy5J
+b7yHm3YM3Xi+8sl5E/uJ+VldTj9KqbD/VIQOs1EX3TEPeObKjfQ/4YIFeRagbAo5
+0IcP6bgh+g7V6aA+Sm9Ui2mLLSpIgN8hPig0796CabhGMW4eVabKx7pstDgdsNd0
+YOpduE8CgYEAu9k9WOQuRX4f6i5LBIxyaYn6Hw6oJn8e/w+p2+HNBXdyVQNqUHBG
+V5rgnBwhc5LeIFbehKvQOvYSWbwbA1VunMpdYgV6+EBLayumJNqV6jGei4okx2of
+wrw7im4TNSAdwVX4Y1F4svJ2as5SJn5QYGAzXDixNuwzXYrpP9rzA2s=
+-----END RSA PRIVATE KEY-----`
+		// create secret
+		err := s.NewSecret(secretName, cert, key)
+		assert.Nil(ginkgo.GinkgoT(), err, "create secret error")
+		// create ApisixTls resource
+		tlsName := "tls-name"
+		host := "api6.com"
+		assert.Nil(ginkgo.GinkgoT(), s.NewApisixTls(tlsName, host, secretName), "create tls error")
 		time.Sleep(6 * time.Second)
 		// status should be recorded as successfulen
-		output, err = s.GetOutputFromString("au", "test-apc", "-o", "yaml")
+		output, err := s.GetOutputFromString("atls", tlsName, "-o", "yaml")
 		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixPluginConfig resource")
-		hasType = strings.Contains(output, "type: ResourceSyncAborted")
-		assert.True(ginkgo.GinkgoT(), hasType, "Status is recorded")
-		hasMsg = strings.Contains(output, "synced failed, with error")
-		assert.True(ginkgo.GinkgoT(), hasMsg, "Status is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourcesSynced", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "True"`, "status.conditions.status  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "message: Sync Successfully", "status.conditions.message  is recorded")
+
+		// No secret exists for use
+		assert.Nil(ginkgo.GinkgoT(), s.NewApisixTls(tlsName, host, "non-existent.com"), "create tls error")
+		time.Sleep(6 * time.Second)
+		// status should be recorded as failed
+		output, err = s.GetOutputFromString("atls", tlsName, "-o", "yaml")
+		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixPluginConfig resource")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourceSyncAborted", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "False"`, "status.conditions.status  is recorded")
 	})
+
+	//TODO: ApisixGlobal
+	//TODO: ApisixUpstream CRD missing status definition
+	//TODO: ApisixConsumer  CRD missing status definition
+	//TODO: ApisixClusterConfig CRD missing status definition
 })
 
 var _ = ginkgo.Describe("suite-ingress-features: Ingress LB Status Testing", func() {
