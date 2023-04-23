e4bcc97716d6d12005499d39f6ef9e1d6f194671
Chever John <cheverjonathan@gmail.com>
2023-04-23 01:13:47 +0000

    test: e2e test for Apisix Cluster Config status

--- test/e2e/suite-ingress/suite-ingress-features/status.go
+++ test/e2e/suite-ingress/suite-ingress-features/status.go
@@ -275,6 +275,20 @@ wrw7im4TNSAdwVX4Y1F4svJ2as5SJn5QYGAzXDixNuwzXYrpP9rzA2s=
 		assert.Contains(ginkgo.GinkgoT(), output, `status: "False"`, "status.conditions.status  is recorded")
 	})
 
+	ginkgo.It("check ApisixClusterConfig status is recorded", func ()  {
+		// create ApisixClusterConfig resource
+		clusterConfigName := "cluster-config-name"
+		assert.Nil(ginkgo.GinkgoT(), s.NewApisixClusterConfig(clusterConfigName, true, true), "create cluster config error")
+		time.Sleep(6 * time.Second)
+
+		// status should be recorded as successfulen
+		output, err := s.GetOutputFromString("acc", clusterConfigName, "-o", "yaml")
+		assert.Nil(ginkgo.GinkgoT(), err, "Get output of ApisixPluginConfig resource")
+		assert.Contains(ginkgo.GinkgoT(), output, "type: ResourcesAvailable", "status.conditions.type is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "reason: ResourcesSynced", "status.conditions.reason  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, `status: "True"`, "status.conditions.status  is recorded")
+		assert.Contains(ginkgo.GinkgoT(), output, "message: Sync Successfully", "status.conditions.message  is recorded")
+	})
 	//TODO: ApisixGlobal
 	//TODO: ApisixConsumer  CRD missing status definition
 	//TODO: ApisixClusterConfig CRD missing status definition
