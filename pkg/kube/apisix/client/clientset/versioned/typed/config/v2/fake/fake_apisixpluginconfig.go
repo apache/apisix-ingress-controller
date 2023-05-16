37decdaeda1515e92298dc89bbfa661a69b3ca58
Jintao Zhang <zhangjintao9020@gmail.com>
2023-05-16 15:28:53 +0800

    chore(deps): bump k8s.io/code-generator from 0.26.3 to 0.27.1
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- pkg/kube/apisix/client/clientset/versioned/typed/config/v2/fake/fake_apisixpluginconfig.go
+++ pkg/kube/apisix/client/clientset/versioned/typed/config/v2/fake/fake_apisixpluginconfig.go
@@ -23,7 +23,6 @@ import (
 	v2 "github.com/apache/apisix-ingress-controller/pkg/kube/apisix/apis/config/v2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -35,9 +34,9 @@ type FakeApisixPluginConfigs struct {
 	ns   string
 }
 
-var apisixpluginconfigsResource = schema.GroupVersionResource{Group: "apisix.apache.org", Version: "v2", Resource: "apisixpluginconfigs"}
+var apisixpluginconfigsResource = v2.SchemeGroupVersion.WithResource("apisixpluginconfigs")
 
-var apisixpluginconfigsKind = schema.GroupVersionKind{Group: "apisix.apache.org", Version: "v2", Kind: "ApisixPluginConfig"}
+var apisixpluginconfigsKind = v2.SchemeGroupVersion.WithKind("ApisixPluginConfig")
 
 // Get takes name of the apisixPluginConfig, and returns the corresponding apisixPluginConfig object, and an error if there is any.
 func (c *FakeApisixPluginConfigs) Get(ctx context.Context, name string, options v1.GetOptions) (result *v2.ApisixPluginConfig, err error) {
