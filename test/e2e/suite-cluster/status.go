dd922abd49015f2d1291edb225a48b96cc8565e4
Chever John <cheverjonathan@gmail.com>
2023-05-18 21:32:17 +0800

    fix: the ci error

--- test/e2e/suite-cluster/status.go
+++ test/e2e/suite-cluster/status.go
@@ -17,7 +17,7 @@ package cluster
 import (
 	"time"
 
-	ginkgo "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
