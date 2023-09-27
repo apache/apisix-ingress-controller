cbeb8644ad19a47dd06b05d9e3ad52d181b09318
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-09-27 11:09:59 +0530

    add comments
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- pkg/providers/ingress/translation/translator.go
+++ pkg/providers/ingress/translation/translator.go
@@ -110,6 +110,7 @@ func (t *translator) TranslateIngressTLS(namespace, ingName, secretName string, 
 	hasher := sha1.New()
 	hasher.Write(tlsByt)
 	uniqueHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	//The name has to be unique per namespace or the IDs generated for Apisix SSL objects will collide
 	apisixTls.ObjectMeta.Name = fmt.Sprintf("%v-%v-%v", ingName, "tls", uniqueHash[:6])
 	return t.ApisixTranslator.TranslateSSLV2(&apisixTls)
 }
