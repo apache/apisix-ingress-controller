25a803e21bf81d0fb1abb06b299912945fecec13
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-03-13 12:16:43 +0530

    fix test

--- test/e2e/suite-features/route_match_exprs.go
+++ test/e2e/suite-features/route_match_exprs.go
@@ -115,13 +115,13 @@ spec:
 			assert.Nil(ginkgo.GinkgoT(), err, "Checking number of upstreams")
 
 			_ = s.NewAPISIXClient().GET("/ip").
-				WithHeader("Host", "httpbin.org").
+				WithHeader("Host", "httpbin.org:80").
 				WithHeader("X-Foo", "bar").
 				Expect().
 				Status(http.StatusOK)
 
 			msg := s.NewAPISIXClient().GET("/ip").
-				WithHeader("Host", "httpbin.org").
+				WithHeader("Host", "httpbin.org:80").
 				WithHeader("X-Foo", "baz").
 				Expect().
 				Status(http.StatusNotFound).
