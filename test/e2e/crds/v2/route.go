d6d5e851777f23c7b7494e7f15349084cd24a0c9
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-07-30 19:44:42 +0530

    fix lint

--- test/e2e/crds/v2/route.go
+++ test/e2e/crds/v2/route.go
@@ -28,7 +28,6 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/assert"
