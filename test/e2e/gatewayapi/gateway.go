cf36af02301597ca968e0df73ee3193088cc50f4
rongxin <rongxin@api7.ai>
2025-08-20 09:40:18 +0800

    fix test

--- test/e2e/gatewayapi/gateway.go
+++ test/e2e/gatewayapi/gateway.go
@@ -216,7 +216,7 @@ spec:
 			assert.ElementsMatch(GinkgoT(), []string{host}, tls[0].Snis)
 		})
 
-		It("Gateway SSL with and without hostname", func() {
+		FIt("Gateway SSL with and without hostname", func() {
 			By("create GatewayProxy")
 			gatewayProxy := fmt.Sprintf(gatewayProxyYaml, s.Namespace(), s.Deployer.GetAdminEndpoint(), s.AdminKey())
 			err := s.CreateResourceFromString(gatewayProxy)
