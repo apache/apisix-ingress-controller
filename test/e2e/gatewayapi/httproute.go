d4bed384c3a0887f071699f77c55d84278200ed3
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-09-09 11:51:53 +0530

    f

--- test/e2e/gatewayapi/httproute.go
+++ test/e2e/gatewayapi/httproute.go
@@ -1701,6 +1701,7 @@ spec:
     - name: httpbin-service-e2e-test
       port: 80
 `
+
 		var corsFilter = `
 apiVersion: gateway.networking.k8s.io/v1
 kind: HTTPRoute
@@ -1716,27 +1717,25 @@ spec:
   - matches:
     - path:
         type: PathPrefix
-        value: /resource/foo
+        value: /headers
     filters:
-    - cors:
-      - type: CORS
+    - type: CORS
+      cors:
         allowOrigins:
         - http://foo.example
-        - http://bar.example
         allowMethods: 
         - GET
-        - HEAD
         - POST
+        - PUT
+        - DELETE
+        - OPTIONS
         allowHeaders: 
-        - Accept
-        - Accept-Language
-        - Content-Language
-        - Content-Type
-        - Range
+        - "*"
     backendRefs:
     - name: httpbin-service-e2e-test
       port: 80
 `
+
 		BeforeEach(beforeEachHTTP)
 
 		It("HTTPRoute RequestHeaderModifier", func() {
@@ -2006,63 +2005,23 @@ spec:
 			})
 		})
 
-		It("HTTPRoute CORS Filter", func() {
+		FIt("HTTPRoute CORS Filter", func() {
 			By("create HTTPRoute with CORS filter")
 			s.ResourceApplied("HTTPRoute", "http-route-cors", fmt.Sprintf(corsFilter, s.Namespace(), s.Namespace()), 1)
-
-			By("test preflight request (OPTIONS)")
+			time.Sleep(time.Hour)
+			By("test simple GET request with CORS headers")
 			s.RequestAssert(&scaffold.RequestAssert{
-				Method: "OPTIONS",
-				Path:   "/resource/foo",
+				Method: "GET",
+				Path:   "/headers",
 				Host:   "httpbin.example",
 				Headers: map[string]string{
-					"Origin":                         "http://foo.example",
-					"Access-Control-Request-Method":  "GET",
-					"Access-Control-Request-Headers": "Content-Type",
+					"Origin": "http://foo.example",
 				},
 				Checks: []scaffold.ResponseCheckFunc{
 					scaffold.WithExpectedStatus(http.StatusOK),
 					scaffold.WithExpectedHeaders(map[string]string{
-						"Access-Control-Allow-Origin":   "http://foo.example",
-						"Access-Control-Allow-Methods":  "GET,HEAD,POST",
-						"Access-Control-Allow-Headers":  "Accept,Accept-Language,Content-Language,Content-Type,Range",
-						"Access-Control-Expose-Headers": "Content-Length,X-Custom-Header",
+						"Access-Control-Allow-Origin": "http://foo.example",
 					}),
-				},
-				Timeout:  time.Second * 30,
-				Interval: time.Second * 2,
-			})
-
-			By("test actual request with CORS headers")
-			s.RequestAssert(&scaffold.RequestAssert{
-				Method: "GET",
-				Path:   "/resource/foo",
-				Host:   "httpbin.example",
-				Headers: map[string]string{
-					"Origin": "http://bar.example",
-				},
-				Checks: []scaffold.ResponseCheckFunc{
-					scaffold.WithExpectedStatus(http.StatusOK),
-					scaffold.WithExpectedHeaders(map[string]string{
-						"Access-Control-Allow-Origin":   "http://bar.example",
-						"Access-Control-Expose-Headers": "Content-Length,X-Custom-Header",
-					}),
-				},
-				Timeout:  time.Second * 30,
-				Interval: time.Second * 2,
-			})
-
-			By("test request from disallowed origin")
-			s.RequestAssert(&scaffold.RequestAssert{
-				Method: "GET",
-				Path:   "/resource/foo",
-				Host:   "httpbin.example",
-				Headers: map[string]string{
-					"Origin": "http://disallowed.example",
-				},
-				Checks: []scaffold.ResponseCheckFunc{
-					scaffold.WithExpectedStatus(http.StatusOK),
-					scaffold.WithExpectedNotHeader("Access-Control-Allow-Origin"),
 				},
 				Timeout:  time.Second * 30,
 				Interval: time.Second * 2,
