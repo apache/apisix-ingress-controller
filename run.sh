9b941feba327732bae343bcf5b058ce76e654e26
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-09-23 22:54:16 +0530

    f

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker container rm -f $(docker container ps -aq); make kind-up; make install; kind load docker-image --name apisix-ingress-cluster \
-  apache/apisix-ingress-controller:dev;  make ginkgo-e2e-test
